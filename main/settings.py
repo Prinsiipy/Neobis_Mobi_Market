c8026c71f407de2d587c3239dccf3744577fddb9
Prinsiipy <chomoevisa@gmail.com>
2023-06-28 22:28:22 +0600

    Add

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [142.93.167.157]
+ALLOWED_HOSTS = ["142.93.167.157"]
 
 # Application definition
 
